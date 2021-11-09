691cc215eed3144ef7523df770a92835f23d1e4f
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-09 07:24:06 +0100

    more changes

--- 05/src/hello/app.cljs
+++ 05/src/hello/app.cljs
@@ -1,5 +1,5 @@
 ;(ns hello.app)
 (ns ^:figwheel-hooks hello.app)
-;(defn ^:after-load re-render []
-;  (js/console.log "after load rerender"))
+(defn ^:after-load re-render []
+  (js/console.log "after load rerender"))
 (js/console.log "hello")
