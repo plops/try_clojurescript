1e6d4938684f2017708772698d70af6b14d4b066
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-10 04:22:02 +0100

    shadow-cljs

--- 07/src/app/main.cljs
+++ 07/src/app/main.cljs
@@ -1,32 +1,34 @@
-;(ns hello.app)
-(ns ^:figwheel-hooks hello.app
-  (:require [reagent.dom :as rdom]))
+(ns app.main)
+;; (ns ^:figwheel-hooks hello.app
+;;   (:require [reagent.dom :as rdom]))
 
-(defn ^:after-load re-render []
-  (js/console.log "after load rerender"))
+;; (defn ^:after-load re-render []
+;;   (js/console.log "after load rerender"))
 
+(defn main! []
+  (println "[main]: loading"))
 
-(js/console.log "hello")
+(defn ^:dev/after-load reload! []
+  (println "[main]: reload"))
 
+;; (defn theme-toggle-field []
+;;   [:input {:type "checkbox"
+;;            :class-name "theme-switch"
+;;            :id "theme-toggle"}])
 
-(defn theme-toggle-field []
-  [:input {:type "checkbox"
-           :class-name "theme-switch"
-           :id "theme-toggle"}])
+;; (defn theme-toggle-label []
+;;   [:label {:class-name "switch-label"
+;;            :for "theme-toggle"}])
 
-(defn theme-toggle-label []
-  [:label {:class-name "switch-label"
-           :for "theme-toggle"}])
+;; (defn page []
+;;   [:<>
+;;    [theme-toggle-field]
+;;    [:div {:id "page"}
+;;     [theme-toggle-label]]])
 
-(defn page []
-  [:<>
-   [theme-toggle-field]
-   [:div {:id "page"}
-    [theme-toggle-label]]])
+;; (defn main-element []
+;;   (-> js/document (.getElementById "app")))
 
-(defn main-element []
-  (-> js/document (.getElementById "app")))
-
-(defn ^:export main []
-  (rdom/render [page]
-               (main-element)))
+;; (defn ^:export main []
+;;   (rdom/render [page]
+;;                (main-element)))
