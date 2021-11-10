ffad37d0df9e8293779fc358363310a7aa674e81
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-10 05:03:01 +0100

    try to use reagent

--- 08/src/app/main.cljs
+++ 08/src/app/main.cljs
@@ -1,9 +1,8 @@
-(ns app.main)
-;; (ns ^:figwheel-hooks hello.app
-;;   (:require [reagent.dom :as rdom]))
+(ns app.main
+  (:require [reagent.dom :as rdom]
+            ;[re-frame.core :as rf]
+            ))
 
-;; (defn ^:after-load re-render []
-;;   (js/console.log "after load rerender"))
 
 (defn main! []
   (println "[main]: loading"))
@@ -11,24 +10,24 @@
 (defn ^:dev/after-load reload! []
   (println "[main]: reload"))
 
-;; (defn theme-toggle-field []
-;;   [:input {:type "checkbox"
-;;            :class-name "theme-switch"
-;;            :id "theme-toggle"}])
+(defn theme-toggle-field []
+  [:input {:type "checkbox"
+           :class-name "theme-switch"
+           :id "theme-toggle"}])
 
-;; (defn theme-toggle-label []
-;;   [:label {:class-name "switch-label"
-;;            :for "theme-toggle"}])
+(defn theme-toggle-label []
+  [:label {:class-name "switch-label"
+           :for "theme-toggle"}])
 
-;; (defn page []
-;;   [:<>
-;;    [theme-toggle-field]
-;;    [:div {:id "page"}
-;;     [theme-toggle-label]]])
+(defn page []
+  [:<>
+   [theme-toggle-field]
+   [:div {:id "page"}
+    [theme-toggle-label]]])
 
-;; (defn main-element []
-;;   (-> js/document (.getElementById "app")))
+(defn main-element []
+  (-> js/document (.getElementById "app")))
 
-;; (defn ^:export main []
-;;   (rdom/render [page]
-;;                (main-element)))
+(defn ^:export main []
+  (rdom/render [page]
+               (main-element)))
