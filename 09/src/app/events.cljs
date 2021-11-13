93db1550fdef4ace0bd34307ca32fda0ebd6a6a8
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-14 00:21:40 +0100

    follow the video

--- 09/src/app/events.cljs
+++ 09/src/app/events.cljs
@@ -3,7 +3,7 @@
    [re-frame.core :as rf]
  ;  [app.db :as db]
    [day8.re-frame.http-fx]
-   ;[ajax.core :as ajax]
+   [ajax.core :as ajax] ;; for use in response-format
   ; [day8.re-frame.tracing :refer-macros [fn-traced]]
    ))
 
@@ -20,7 +20,14 @@
  (fn [db [_ val]]
    (assoc db :name val)))
 
-;; (comment 
-;;   (rf/reg-event-fx
-;;   ::fetch-users
-;;   (fn [{:keys [db]} _])))
+;; from https://github.com/day8/re-frame-http-fx
+(reg-event-fx                             ;; note the trailing -fx
+ :handler-with-http                      ;; usage:  (dispatch [:handler-with-http])
+ (fn [{:keys [db]} _]                    ;; the first param will be "world"
+   {:db   (assoc db :show-twirly true)   ;; causes the twirly-waiting-dialog to show??
+    :http-xhrio {:method          :get
+                 :uri             "https://api.github.com/orgs/day8"
+                 :timeout         8000                                           ;; optional see API docs
+                 :response-format (ajax/json-response-format {:keywords? true})  ;; IMPORTANT!: You must provide this.
+                 :on-success      [:good-http-result]
+                 :on-failure      [:bad-http-result]}}))
