a405832a0013809d3ae26e6dc385e6666942b7bb
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-11-14 00:03:35 +0100

    typo

--- 09/src/app/events.cljs
+++ 09/src/app/events.cljs
@@ -1,16 +1,18 @@
 (ns app.events
   (:require
    [re-frame.core :as rf]
-   [app.db :as db]
-   [day8.re-frame/http-fx]
+ ;  [app.db :as db]
+   [day8.re-frame.http-fx]
    ;[ajax.core :as ajax]
-   [day8.re-frame.tracing :refer-macros [fn-traced]]
+  ; [day8.re-frame.tracing :refer-macros [fn-traced]]
    ))
 
-(rf/reg-event-db
- ::initialize-db
- (fn-traced [_ _]
-            db/default-db))
+;; (comment
+;;   (rf/reg-event-db
+;;    ::initialize-db
+;;    (fn-traced [_ _]
+;;               db/default-db)))
+
 
 
 (rf/reg-event-db
@@ -18,7 +20,7 @@
  (fn [db [_ val]]
    (assoc db :name val)))
 
-(comment
-  (rf/reg-event-fx
-  ::fetch-users
-  (fn [{:keys [db]} _])))
+;; (comment 
+;;   (rf/reg-event-fx
+;;   ::fetch-users
+;;   (fn [{:keys [db]} _])))
