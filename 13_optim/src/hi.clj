fc1032e8fc7f30849a511bc4ad53f59b16981fa2
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-01 22:34:10 +0100

    fitting seems to work

--- 13_optim/src/hi.clj
+++ 13_optim/src/hi.clj
@@ -6,4 +6,16 @@
 
 (let [obs (WeightedObservedPoints.)]
   (.add obs 4.024 531026.0)
-  obs)
+  (.add obs 4.05325 3580526.0)
+  (.add obs 4.06943 2175960.0)
+  (.add obs 4.07525 1447024.0)
+  (.add obs 4.0836 620014.0)
+  (let [params (-> (GaussianCurveFitter/create)
+                   (.fit (.toList obs)))]
+    (list (aget params 0)
+          (aget params 1)
+          (aget params 2)))
+  )
+
+(type GaussianCurveFitter)
+
