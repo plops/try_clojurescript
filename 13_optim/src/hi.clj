047601cd1034f610bd07e5446ea09cc653a052e1
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-01 22:37:39 +0100

    also runs from command line
    [martin@localhost 13_optim]$ clj -m hi
    WARNING: Implicit use of clojure.main with options is deprecated, use -M
    fit result: (3575197.644499728 4.054178197204313 0.015551723422184114)

--- 13_optim/src/hi.clj
+++ 13_optim/src/hi.clj
@@ -2,9 +2,7 @@
   (:import (org.apache.commons.math3.fitting GaussianCurveFitter WeightedObservedPoints)))
 
 (defn -main [& _]
-  (println "hello"))
-
-(let [obs (WeightedObservedPoints.)]
+  (let [obs (WeightedObservedPoints.)]
   (.add obs 4.024 531026.0)
   (.add obs 4.05325 3580526.0)
   (.add obs 4.06943 2175960.0)
@@ -12,10 +10,24 @@
   (.add obs 4.0836 620014.0)
   (let [params (-> (GaussianCurveFitter/create)
                    (.fit (.toList obs)))]
-    (list (aget params 0)
-          (aget params 1)
-          (aget params 2)))
-  )
+    (println "fit result:" (list (aget params 0)
+                                 (aget params 1)
+                                 (aget params 2))))
+  ))
 
-(type GaussianCurveFitter)
+(comment
+ (let [obs (WeightedObservedPoints.)]
+   (.add obs 4.024 531026.0)
+   (.add obs 4.05325 3580526.0)
+   (.add obs 4.06943 2175960.0)
+   (.add obs 4.07525 1447024.0)
+   (.add obs 4.0836 620014.0)
+   (let [params (-> (GaussianCurveFitter/create)
+                    (.fit (.toList obs)))]
+     (list (aget params 0)
+           (aget params 1)
+           (aget params 2)))
+   ))
+(comment
+ (type GaussianCurveFitter))
 
