4a28f9782f31841fb20715372319cc478bedd796
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-01 23:06:47 +0100

    try some reflection
    |                       :name |             :return-type |            :declaring-class |                                                                                    :parameter-types | :exception-types |             :flags |
    |-----------------------------+--------------------------+-----------------------------+-----------------------------------------------------------------------------------------------------+------------------+--------------------|
    |                    getChart | org.knowm.xchart.XYChart | org.knowm.xchart.QuickChart |         [java.lang.String java.lang.String java.lang.String java.lang.String<> double<> double<><>] |               [] | #{:public :static} |
    |                    getChart | org.knowm.xchart.XYChart | org.knowm.xchart.QuickChart | [java.lang.String java.lang.String java.lang.String java.lang.String java.util.List java.util.List] |               [] | #{:public :static} |
    |                    getChart | org.knowm.xchart.XYChart | org.knowm.xchart.QuickChart |             [java.lang.String java.lang.String java.lang.String java.lang.String double<> double<>] |               [] | #{:public :static} |
    | org.knowm.xchart.QuickChart |                          | org.knowm.xchart.QuickChart |                                                                                                  [] |               [] |        #{:private} |

--- 13_optim/src/hi.clj
+++ 13_optim/src/hi.clj
@@ -1,5 +1,6 @@
 (ns hi
-  (:import (org.apache.commons.math3.fitting GaussianCurveFitter WeightedObservedPoints)))
+  (:import (org.apache.commons.math3.fitting GaussianCurveFitter WeightedObservedPoints)
+           (org.knowm.xchart XYChart QuickChart SwingWrapper)))
 
 (defn -main [& _]
   (let [obs (WeightedObservedPoints.)]
@@ -15,20 +16,36 @@
                                  (aget params 2))))
   ))
 
-(comment
- (let [obs (WeightedObservedPoints.)]
-   (.add obs 4.024 531026.0)
-   (.add obs 4.05325 3580526.0)
-   (.add obs 4.06943 2175960.0)
-   (.add obs 4.07525 1447024.0)
-   (.add obs 4.0836 620014.0)
-   (let [params (-> (GaussianCurveFitter/create)
-                    (.fit (.toList obs)))]
-     (list (aget params 0)
-           (aget params 1)
-           (aget params 2)))
-   ))
-(comment
- (type GaussianCurveFitter))
+(QuickChart/getChart "sample chart"
+                     "x"
+                     "y"
+                     ["y(x)"] 
+                     (into-array ^double (map double [0 1 2]))
+                     [(into-array ^double (map double [2 1 0.1]))]
+                     )
+(type QuickChart/getChart)
 
-`
+(clojure.pprint/print-table
+ (sort-by :name
+  (filter :exception-types
+          (:members (clojure.reflect/reflect QuickChart)))))
+
+
+
+;; (comment
+;;  (let [obs (WeightedObservedPoints.)]
+;;    (.add obs 4.024 531026.0)
+;;    (.add obs 4.05325 3580526.0)
+;;    (.add obs 4.06943 2175960.0)
+;;    (.add obs 4.07525 1447024.0)
+;;    (.add obs 4.0836 620014.0)
+;;    (let [params (-> (GaussianCurveFitter/create)
+;;                     (.fit (.toList obs)))]
+;;      (list (aget params 0)
+;;            (aget params 1)
+;;            (aget params 2)))
+;;    ))
+;; (comment
+;;  (type GaussianCurveFitter))
+
+;; `
