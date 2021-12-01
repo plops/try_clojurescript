e2e86d138a27cb0a1b6527e0487a36997333c428
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-01 22:09:41 +0100

    run.sh seems to start clojure
    WARNING: Implicit use of clojure.main with options is deprecated, use -M
    hello

--- 13_optim/run.sh
+++ 13_optim/run.sh
@@ -1,2 +1,2 @@
 # clj -Spath
-clj -Scp src:/home/martin/.m2/repository/org/clojure/clojure/1.10.3/clojure-1.10.3.jar:/home/martin/.m2/repository/org/clojure/core.specs.alpha/0.2.56/core.specs.alpha-0.2.56.jar:/home/martin/.m2/repository/org/clojure/spec.alpha/0.2.194/spec.alpha-0.2.194.jar:
+clj -Scp src:/home/martin/.m2/repository/org/clojure/clojure/1.10.3/clojure-1.10.3.jar:/home/martin/.m2/repository/org/clojure/core.specs.alpha/0.2.56/core.specs.alpha-0.2.56.jar:/home/martin/.m2/repository/org/clojure/spec.alpha/0.2.194/spec.alpha-0.2.194.jar:/home/martin/stage/try_clojurescript/13_optim/commons-math3-3.6.1/commons-math3-3.6.1.jar -m hi
