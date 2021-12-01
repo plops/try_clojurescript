32d4773ba1aaa655a1725eae1e0f4342aa55623c
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-01 22:28:24 +0100

    it seems to call into the jar file

--- 13_optim/src/hi.clj
+++ 13_optim/src/hi.clj
@@ -1,5 +1,9 @@
 (ns hi
-  (:import (org.apache.commons.math4.fitting GaussianCurveFitter WeightedObservedPoints)))
+  (:import (org.apache.commons.math3.fitting GaussianCurveFitter WeightedObservedPoints)))
 
 (defn -main [& _]
   (println "hello"))
+
+(let [obs (WeightedObservedPoints.)]
+  (.add obs 4.024 531026.0)
+  obs)
