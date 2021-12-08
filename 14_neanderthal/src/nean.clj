e5590076a0113228cc99b096d0f2a92a2bc80b5b
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-08 23:43:21 +0100

    i think  it loaded mkl libraries

--- 14_neanderthal/src/nean.clj
+++ 14_neanderthal/src/nean.clj
@@ -1,6 +1,12 @@
 (ns nean
-  (:require
-   [uncomplicaate.neanderthal.core :as unc :refer (axpy mm scal mv)]
+  (:use
+   [uncomplicate.neanderthal core native])
+ #_ (:require
+   [uncomplicate.neanderthal.core :as unc :refer (axpy mm scal mv)]
+   [uncomplicate.neanderthal.linalg :as unl]
+   [uncomplicate.neanderthal.math :as unm :refer (cos sin)]
+   [uncomplicate.neanderthal.native :as unn :refer (dv dge)]
+   [uncomplicate.neanderthal.random :as unr]
    ))
 
 (def x (dv 1 2 3))
