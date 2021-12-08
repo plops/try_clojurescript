c61141643d494599274b6cd28ef4f2f398deb0aa
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-08 23:49:32 +0100

    play a bit with neanderthal functions

--- 14_neanderthal/src/nean.clj
+++ 14_neanderthal/src/nean.clj
@@ -1,6 +1,10 @@
 (ns nean
+  (:require
+   [uncomplicate.neanderthal.linalg :as unl]
+     )
   (:use
-   [uncomplicate.neanderthal core native])
+   [uncomplicate.neanderthal core native random math])
+  
  #_ (:require
    [uncomplicate.neanderthal.core :as unc :refer (axpy mm scal mv)]
    [uncomplicate.neanderthal.linalg :as unl]
@@ -10,3 +14,20 @@
    ))
 
 (def x (dv 1 2 3))
+
+(def xs [(dv [1 2 3])
+         (dv [1 2 3])
+         (dv 3)])
+
+(defn rand-vec [n]
+  (let [x (rand-uniform! (dv n))]
+    (scal (/ 1 (nrm2 x))
+          x)))
+
+(rand-vec 31)
+
+
+(def zz (rand-normal! (dge 3 2)))
+zz
+
+(axpy x x)
